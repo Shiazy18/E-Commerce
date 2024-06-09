64ac99c314792066bd0b99075eff598e742419ab
Shivam <shivampant0998@gmail.com>
2024-06-09 20:49:02 +0530

    added entity framework nuget packages

---
+++ E-Commerce/Models/category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.Models
+{
+	public class category
+	{
+		[Key]
+		public int ID  { get; set; }
+		[Required]
+		public string CategoryName { get; set; }
+		public object CategotyOrder { get; set; }
+
+	}
+}
+
